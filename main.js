6db66d7a193adb79b16790e3e9c75ac69c06ed16
Jean Christophe Gomis <devdujour@yahoo.com>
2022-02-15 13:25:20 +0100

    Création , stylisation des bulles et logique js

--- main.js
+++ main.js
@@ -1,3 +1,33 @@
-const bubble = document.createElement("span");
-document.body.appendChild(bubble);
-bubble.classList.add('bubble')
+
+//Je crée une fonction qui va englober tout le procéssus de création des bulles
+const bubbleMaker = () => {
+      // Création d'une balise span pour contenir laa bulle
+      const bubble = document.createElement("span");
+      document.body.appendChild(bubble);
+      bubble.classList.add('bubble')
+      //Je donne une dimension à la bulle
+      const size = Math.random() * 200 + 100 + "px";
+      bubble.style.height = size;
+      bubble.style.width = size;
+      //Je régle le positionnement de la bulle
+      bubble.style.top = Math.random() * 100 + 50 + "%"
+      bubble.style.left = Math.random() * 100 + "%"
+      const plusMinus = Math.random() > 0.5 ? 1 : -1
+      //Je vais chercher à toucher ma variable css
+      bubble.style.setProperty('--left', Math.random() * 100 + plusMinus + "%");
+
+      //Faire disparaitre une bulle au click
+
+      bubble.addEventListener('click', () => {
+            bubble.remove();
+      })
+      // Arreter les bulle au bout de 8s avec setTimeout
+      setTimeout(() => {
+            bubble.remove()
+      }, 8000)
+
+
+}
+//Création de plusieurs bulles avec la methode setInterval
+setInterval(bubbleMaker, 1000)
+
